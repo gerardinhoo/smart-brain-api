0d0ba89f05cfea0f6ff84f688aa292a7e5207caf
Gerard Eklu <gerard.eklu@yahoo.fr>
2019-03-14 20:36:50 -0400

    Initial commit

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'a6a30da1aaa849b7a348b5314b0db9e2'
 });
 
 const handleApiCall = (req, res) => {
